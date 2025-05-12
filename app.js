3d541e71b3d9cf435a81b84d4d6a85994179293d
guilhermef-roveri <guilherme.roveri@sptech.school>
2025-05-12 19:55:59 -0300

    Limpeza no código da API

--- app.js
+++ app.js
@@ -17,9 +17,6 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
-var avisosRouter = require("./src/routes/avisos");
-var medidasRouter = require("./src/routes/medidas");
-var empresasRouter = require("./src/routes/empresas");
 var treinoRouter = require("./src/routes/treino");
 
 app.use(express.json());
@@ -30,9 +27,6 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
-app.use("/avisos", avisosRouter);
-app.use("/medidas", medidasRouter);
-app.use("/empresas", empresasRouter);
 app.use("/treino", treinoRouter)
 
 app.listen(PORTA_APP, function () {
