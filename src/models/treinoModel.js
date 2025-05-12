d36820abbe8a85ead0b9bc43c533938f90e67045
guilhermef-roveri <guilherme.roveri@sptech.school>
2025-05-12 19:52:38 -0300

    Tela de Inicio Quiz feita, nav da Dashboard funcionando

--- src/models/treinoModel.js
+++ src/models/treinoModel.js
@@ -2,7 +2,7 @@ var database = require("../database/config")
 
 function cadastrar(horasTreino){
     var instrucao = ` 
-    INSERT INTO treino(fkUsuario,dia,tempoTreino) VALUES (1,'2025-05-10','${horasTreino}');
+    INSERT INTO treino(fkUsuario,dia,tempoTreino) VALUES (1,CURDATE(),'${horasTreino}');
     `;
     console.log("Inserindo o tempo de treino no SQL: \n" + horasTreino);
     return database.executar(instrucao)
