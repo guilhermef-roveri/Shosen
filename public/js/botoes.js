8a97ea73f2c2c45a1be115019e325f5feb318d4c
guilhermef-roveri <guilherme.roveri@sptech.school>
2025-05-14 09:37:46 -0300

    Session Storage sendo limpo, nome aparecendo e banco inserindo no id correto. Organização no meu JS

---
+++ public/js/botoes.js
@@ -0,0 +1,11 @@
+function botaoHome() {
+        window.location.href = "index.html"
+    }
+
+    function cadastro(){
+    window.location.href = "cadastro.html"
+}
+
+   function Login(){
+      window.location.href = "login.html"
+    }
