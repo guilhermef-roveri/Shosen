3b0d4550bf60ee6cbd14d42b0fb977a430135d3c
guilhermef-roveri <guilherme.roveri@sptech.school>
2025-06-06 13:32:33 -0300

    Botoes Footer funcionando, botao home funcionando e imagens adicionadas

--- modelagemBancoDeDados/scriptShosen.sql
+++ modelagemBancoDeDados/scriptShosen.sql
@@ -15,25 +15,9 @@ idTreino INT PRIMARY KEY AUTO_INCREMENT,
 fkUsuario INT,
 dia DATE,
 tempoTreino INT,
-CONSTRAINT fkUsuario FOREIGN KEY(fkUsuario) REFERENCES usuario(idUsuario)
+CONSTRAINT fkUsuario FOREIGN KEY(fkUsuario) REFERENCES usuario(idUsuario),
+CONSTRAINT unqUsuarioPorDia UNIQUE(fkUsuario,dia)
 );
-
-INSERT INTO usuario (nome, email, senha)
-VALUES ('Ana Silva', 'ana.silva@email.com', 'senha123'),
- ('Bruno Costa', 'bruno.costa@email.com', 'abc12345'),
- ('Carla Mendes', 'carla.mendes@email.com', 'minhasenha'),
- ('Diego Rocha', 'diego.rocha@email.com', '12345678');
-
-INSERT INTO treino (fkUsuario, dia, tempoTreino)
-VALUES (1, '2025-05-10', 1),
- (2, '2025-05-11', 2),
- (3, '2025-05-12', 3);
-SELECT * FROM usuario;	
-
-
-SELECT * FROM treino;
-
-INSERT INTO treino(fkUsuario,dia,tempoTreino) VALUES(1,Curdate(),3);
 
 -- Criacao tabela do quiz
 CREATE TABLE acertosQuiz(
@@ -46,37 +30,6 @@ PRIMARY KEY(idResultado,fkUsuario)
 );
 
 
-INSERT INTO usuario (nome, email, senha) VALUES 
-('Ana Silva', 'ana.silva@example.com', 'senha123'),
-('Bruno Costa', 'bruno.costa@example.com', 'abc456'),
-('Carla Mendes', 'carla.mendes@example.com', 'carla789'),
-('Daniel Souza', 'daniel.souza@example.com', 'daniel321'),
-('Eduarda Lima', 'eduarda.lima@example.com', 'edu123'),
-('Felipe Rocha', 'felipe.rocha@example.com', 'felipe456'),
-('Gabriela Teixeira', 'gabriela.teixeira@example.com', 'gabi789'),
-('Henrique Alves', 'henrique.alves@example.com', 'henrique321');
-
-INSERT INTO treino (fkUsuario, dia, tempoTreino) VALUES 
-(1, '2025-05-10', 45),
-(2, '2025-05-11', 30),
-(3, '2025-05-12', 60),
-(4, '2025-05-13', 40),
-(5, '2025-05-14', 35),
-(6, '2025-05-15', 50),
-(7, '2025-05-16', 55),
-(8, '2025-05-17', 25);
-
-
-
-INSERT INTO acertosQuiz (fkUsuario, acertos, dia) VALUES
-(1, 8, '2025-05-10'),
-(2, 6, '2025-05-11'),
-(3, 9, '2025-05-12'),
-(4, 7, '2025-05-13'),
-(5, 5, '2025-05-14'),
-(6, 10, '2025-05-15'),
-(7, 4, '2025-05-16'),
-(8, 6, '2025-05-17');
 
 SELECT SUM(tempoTreino) as TempoTreinado FROM treino
 INNER JOIN usuario on idUsuario = fkUsuario WHERE idUsuario = 3; -- Horas de treino totais do usuário
@@ -90,3 +43,15 @@ INNER JOIN usuario on idUsuario = fkUsuario WHERE idUsuario = 3; -- Média de tr
 -- Horas de treino do usuário
 
 SELECT * FROM treino;
+
+
+SELECT DAY(dia) FROM treino 
+INNER JOIN  usuario on idUsuario = fkUsuario
+WHERE idUsuario = 3; -- Dias da semana de treino do usuário
+
+SELECT COUNT(tempoTreino), nome, DAY(dia) FROM treino
+INNER JOIN usuario on idUsuario = fkUsuario
+GROUP BY nome, dia; -- Tempo de treino
+
+SELECT * FROM treino 
+INNER JOIN usuario on idUsuario = fkUsuario WHERE idUsuario = 3;  	
